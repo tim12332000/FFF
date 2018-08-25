663c7528a2660db274cc1fb13ea02fdd552807dc
FSISF <a1455785@gmail.com>
2018-08-25 16:05:08 +0800

    Game UI Value
    Game UI Value

---
+++ FFF/Assets/Scripts/UI/GameUIValue.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUIValue : SingletonMono<GameUIValue>
+{
+    private int life = 3;
+    public int Life
+    {
+        get
+        {
+            return life;
+        }
+        set
+        {
+            life = value;
+
+            for (int i = 0; i < ImagesLife.Length; i++)
+            {
+                ImagesLife[i].color = Color.clear;
+            }
+            for (int i = 0; i < life; i++)
+            {
+                ImagesLife[i].color = Color.white;
+            }
+        }
+    }
+    public Image[] ImagesLife;
+    //============================================================
+    private int time = 0;
+    public int Time
+    {
+        get
+        {
+            return time;
+        }
+        set
+        {
+            time = value;
+            TextTime.text = time.ToString();
+        }
+    }
+    public Text TextTime = null;
+    //============================================================
+    private int far = 0;
+    public int Far
+    {
+        get
+        {
+            return far;
+        }
+        set
+        {
+            far = value;
+            TextFar.text = far.ToString();
+        }
+    }
+    public Text TextFar = null;
+    //============================================================
+
+    void Awake()
+    {
+        Life = 3;
+        Time = 0;
+        Far = 0;
+    }
+
+    void Start()
+    {
+
+    }
+}

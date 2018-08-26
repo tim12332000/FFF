da2adfa66bed76ee13ab7dda5dffd77de751229f
Hayato <hayatos@gmail.com>
2018-08-26 10:27:52 +0800

    Scroll Script Control Warper

---
+++ FFF/Assets/[Hayato]/GroundScrollController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HutongGames.PlayMaker;
+
+public class GroundScrollController : MonoBehaviour {
+
+    public string eventName = "GO";
+    public PlayMakerFSM targetFSM;
+    public float ScrollSpeed;
+
+	// Use this for initialization
+	void ScrollStart() {
+
+        targetFSM.SendEvent(eventName);
+
+	}
+
+}
